fd80afb3971f61f1669ca0793c7f4d83a84a2bdb
Meghna Sahu <me.ghnasahu0015@gmail.com>
2025-07-12 18:13:25 +0530

    ExampleUnitTest.kt

--- app/src/test/java/com/example/to-docompose/ExampleUnitTest.kt
+++ app/src/test/java/com/example/to-docompose/ExampleUnitTest.kt
@@ -4,9 +4,6 @@ import org.junit.Test
 
 import org.junit.Assert.*
 
-/**
- * Example local unit test, which will execute on the development machine (host).
- */
 class ExampleUnitTest {
     @Test
     fun addition_isCorrect() {
